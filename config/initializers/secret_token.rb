7610d9f6879390ebbf20602fc620b598d17e014b
Peter Abplanalp <peter@abplanalps.net>
2013-03-20 08:40:04 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myprimaltools::Application.config.secret_token = 'c37cb3a8d3b1585f70b31adff23963be6b02dd6ae67cf17e48c68eea5eda56482d190a3c65c31f241bf40475e03ca4025cee7e253db4b79a7288109ea7d72497'
